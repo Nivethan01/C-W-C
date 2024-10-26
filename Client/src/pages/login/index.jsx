5dc4703f79fb01fdac4dbc691148a069964757f5
Nivethan Eswaran <140292846+Nivethan01@users.noreply.github.com>
2024-10-26 21:21:17 +0530

    Auth Data

--- Client/src/pages/login/index.jsx
+++ Client/src/pages/login/index.jsx
@@ -1,4 +1,44 @@
-function Login(){
-    return (<h2>This is Login Page</h2>)
+import React from "react";
+import { Link } from "react-router-dom";
+function Login() {
+  const [User, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  function onSubmit(e){
+    e.preventDefault();
+    console.log(e);
+    
+  }
+  return (
+    <div className="container">
+      <div className="container-back-img"></div>
+      <div className="container-back-color"></div>
+      <div className="card">
+        <div className="card_title">
+          <h1>Login Here</h1>
+        </div>
+        <div className="form">
+          <form onSubmit={onSubmit}>
+            <input type="email" placeholder="Email" 
+            value={User.email}
+            onChange={(e)=>setUser({...User,email:e.target.value})}
+            />
+            <input type="password" placeholder="Password"
+            value={User.password}
+            onChange={(e)=>setUser({...User,password:e.target.value})}
+            />
+            <button >Login</button>
+          </form>
+        </div>
+        <div className="card_terms">
+          <span>
+            Don't have an account yet?
+            <Link to="/signup">Signup Here</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Login;
--- Client/src/pages/signUp/index.jsx
+++ Client/src/pages/signUp/index.jsx
@@ -1,4 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
 function SignUp() {
+  const [User, setUser] = React.useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+  function onformSubmit(e) {
+    e.preventDefault();
+    console.log(User);
+  }
   return (
     <div className="container">
       <div className="container-back-img"></div>
@@ -8,20 +20,42 @@ function SignUp() {
           <h1>Create Account</h1>
         </div>
         <div className="form">
-          <form>
+          <form onSubmit={onformSubmit}>
             <div className="column">
-              <input type="text" placeholder="First Name" />
-              <input type="text" placeholder="Last Name" />
+              <input
+                type="text"
+                placeholder="First Name"
+                value={User.firstname}
+                onChange={(e) =>
+                  setUser({ ...User, firstname: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                value={User.lastname}
+                onChange={(e) => setUser({ ...User, lastname: e.target.value })}
+              />
             </div>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input
+              type="email"
+              placeholder="Email"
+              value={User.email}
+              onChange={(e) => setUser({ ...User, email: e.target.value })}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={User.password}
+              onChange={(e) => setUser({ ...User, password: e.target.value })}
+            />
             <button>Sign Up</button>
           </form>
         </div>
         <div className="card_terms">
           <span>
             Already have an account?
-            <a>Login Here</a>
+            <Link to="/login">Login Here</Link>
           </span>
         </div>
       </div>
